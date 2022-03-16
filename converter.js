0304161f97ea82ff01358781594f438df662baa7
Rafael Estevam <rafael_estevan15@hotmail.com>
2022-03-16 13:51:14 -0300

    Removendo bandeiras

--- converter.js
+++ converter.js
@@ -1,63 +1,74 @@
+var flagList = [
+	"https://flagcdn.com/16x12/us.png",
+	"https://flagcdn.com/16x12/eu.png",
+	"https://flagcdn.com/16x12/gb.png"
+];
+
 var moeda = atualizaMoeda();
 var elemento = document.getElementById("valor");
 
+function atualizaMoeda() {
+	let select = document.querySelector("#seletorMoeda");
+	let opçãoMoeda = select.options[select.selectedIndex];
 
-function atualizaMoeda(){
-  let select = document.querySelector("#seletorMoeda");
-  let opçãoMoeda = select.options[select.selectedIndex];
-
-  moeda = opçãoMoeda.value;
+	moeda = opçãoMoeda.value;
   return moeda;
+  //flag.innerHTML = `<img src="${flagList[moeda-1]}" >`
 }
 
-function Converter(){
-
-  switch (moeda) {
-    case "1": converterUs(); break;
-    case "2": converterEu(); break;
-    case "3": converterGb(); break;
-  }
+function Converter() {
+	switch (moeda) {
+		case "1":
+			converterUs();
+			break;
+		case "2":
+			converterEu();
+			break;
+		case "3":
+			converterGb();
+			break;
+	}
 }
 
 function converterUs() {
-  var input = parseFloat(elemento.value);
-  var output = input / 5.1;
+	var input = parseFloat(elemento.value);
+	var output = input / 5.1;
 
-  const formatter = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits:2,
-  })
+	const formatter = new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "USD",
+		minimumFractionDigits: 2
+	});
 
-  document.getElementById("valorConvertido").value=formatter.format(output);
+	document.getElementById("valorConvertido").value = formatter.format(output);
 }
 
 function converterEu() {
-  var input = parseFloat(elemento.value);
-  var output = input / 5.6;
+	var input = parseFloat(elemento.value);
+	var output = input / 5.6;
 
-  const formatter = new Intl.NumberFormat("pt-BR", {
-    style:"currency",
-    currency:"EUR",
-    minimumFractionDigits:2,
-  })
+	const formatter = new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "EUR",
+		minimumFractionDigits: 2
+	});
 
-  document.getElementById("valorConvertido").value=formatter.format(output);
+	document.getElementById("valorConvertido").value = formatter.format(output);
 }
 
 function converterGb() {
-  var input = parseFloat(elemento.value);
-  var output = input / 6.7;
+	var input = parseFloat(elemento.value);
+	var output = input / 6.7;
 
-  const formatter = new Intl.NumberFormat("pt-BR", {
-    style:"currency",
-    currency:"GBP",
-    minimumFractionDigits:2,
-  })
+	const formatter = new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "GBP",
+		minimumFractionDigits: 2
+	});
 
-  document.getElementById("valorConvertido").value=formatter.format(output);
+	document.getElementById("valorConvertido").value = formatter.format(output);
 }
 
 function Limpar (){
-  document. location. reload()
-}
+	document. location. reload()
+  }
